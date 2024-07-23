14367b56408fa45e16ef1874bff7f9d23f4bdf28
RYAN <ryancpotts@gmail.com>
2024-07-23 14:07:35 -0500

    caps done

---
+++ driver/driver.js
@@ -0,0 +1,17 @@
+'use strict';
+
+require('dotenv').config();
+const HUB_URL = process.env.HUB_URL || 'http://localhost:3000';
+
+const io = require('socket.io-client');
+const socket = io.connect(HUB_URL);
+
+socket.on('pickup', (payload) => {
+  console.log('Picking up order:', payload);
+  socket.emit('in-transit', payload);
+
+  setTimeout(() => {
+    console.log('Order delivered', payload);
+    socket.emit('delivered', payload);
+  }, 5000);
+});
